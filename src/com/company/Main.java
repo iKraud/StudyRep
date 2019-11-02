44b1718c416cd84b03cb19e69fc4be28fe296b8f
Igor Timohin <ikraud@gmail.com>
2019-11-02 18:17:36 +0300

    Пробный

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,38 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
-         int i = 2;
+        String hw = "Hello, World!";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1 для NullPointerException");
+        System.out.println("2 для ArrayIndexOutOfBoundsException");
+        System.out.println("3 для сюрприза");
+
+        try {
+            int script = sc.nextInt();
+            switch (script) {
+                case 1:
+                    throw new NullPointerException("demo");
+                    break;
+                case 2:
+                    throw new ArrayIndexOutOfBoundsException("demo");
+                    break;
+                case 3:
+                    break;
+                default:
+
+            }
+        }
+        catch (NullPointerException e) {
+            System.out.println("Пойман NPE!");
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Пойман AIOOBE!");
+        }
+        finally {
+            System.out.println(hw);
+        }
     }
 }

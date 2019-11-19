967a0e74557d3e0f08e1ceeb8d3f0d11d9060fca
iKraud <57291604+iKraud@users.noreply.github.com>
2019-11-19 09:55:51 +0300

    Update Main.java
    Начало ДЗ_6

--- src/hw06Task1/Main.java
+++ src/hw06Task1/Main.java
@@ -1,5 +1,6 @@
 package hw06Task1;
 
+import java.io.*;
 /**
  * @author "Timohin Igor"
  *
@@ -8,8 +9,18 @@ package hw06Task1;
  * Найденные слова не должны повторяться, регистр не должен учитываться. Одно слово в разных падежах – это разные слова.
  */
 
-public class Main {
-    public static void main(String[] args) {
-
+public class Main{
+    public static void main(String []args) throws IOException{
+        // //нужно работать с потоком, не с nio
+        // nio //пример из лекции
+        // strList = Files.readAllFiles(Paths.get(note.txt)); //пример из лекции
+        // strList.forEach(System.out::println); //пример из лекции
+        
+        byte[] arr = "Hello МИР!".getBytes();
+        InputStream byteArrayInputStream = new ByteArrayInputStream (arr);
+        int b;
+        while ((b = byteArrayInputStream.read()) != -1) {
+            System.out.print((char)b);
+        }
     }
 }

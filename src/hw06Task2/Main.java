5f73eaead58b6875d087472fffbb985d2c5ecf61
iKraud <57291604+iKraud@users.noreply.github.com>
2019-11-20 15:56:50 +0300

    Работа с ДЗ_6 задача 2

--- src/hw06Task2/Main.java
+++ src/hw06Task2/Main.java
@@ -15,14 +15,16 @@ import java.util.*;
 Есть массив слов 1<=n4<=1000.
 Есть вероятность probability вхождения одного из слов этого массива в следующее предложение (1/probability).
 Необходимо написать метод getFiles(String path, int n, int size, String[] words, int probability),
-который создаст n файлов размером size в каталоге path. words - массив слов, probability - вероятность.
+который создаст n файлов размером size в каталоге path. words - массив слов, probability - вероятность. ??????????????
  */
 
 public class Main {
     public static void main(String[] args) {
         Random rnd = new Random();
         int lettersInWord; //латинских букв в слове 1<=n2<=15
-        int probability = 10; //вероятность вхождения одного из слов массива в следующее предложение (1/probability)
+        int sentenceInParagrarh; //предложений в абзаце 1<=n3<=20
+        int wordsInSentence; //слов в предложении 1<=n1<=15
+        int probability = 3; //вероятность вхождения одного из слов массива в следующее предложение (1/probability)
         int amountOfFiles = 10; // файлов нужно сформировать
         // String path = "Путь:\\" //путь для сохранения файлов
 
@@ -36,46 +38,63 @@ public class Main {
             for (int j = 0; j < lettersInWord; j++) {
                 word += String.valueOf(Character.forDigit(rnd.nextInt(35-10)+10,36));
             }
-            additionalArray.add(word);
-        }
-        for (String el : additionalArray) {
-            System.out.println(el);
+            additionalArray.add(word.toUpperCase()); //upperCase - чтобы отличить основные слова от дополнительных
         }
         // генерация дополнительного массива слов
 
         // создание параграфов
         List<String> sentenceArray = new ArrayList<>();
-        int wordsInSentence = rnd.nextInt(14)+1; //слов в предложении 1<=n1<=15
-        int sentenceInParagrarh = rnd.nextInt(19)+1; //предложений в абзаце 1<=n3<=20
-        int amountOfParagraph = 10; // количество абзацев getFiles - size
+        int amountOfParagraph = 20; // количество абзацев getFiles - size??????????????
         for (int par = 0; par < amountOfParagraph; par++) {
+            sentenceInParagrarh = rnd.nextInt(19)+1; //предложений в абзаце 1<=n3<=20
             for (int sent = 0; sent < sentenceInParagrarh; sent++) {
+                wordsInSentence = rnd.nextInt(14)+1; //слов в предложении 1<=n1<=15
                 for (int wordCount = 0; wordCount < wordsInSentence; wordCount++) {
                     String word = "";
-                    lettersInWord = rnd.nextInt(14)+1; //латинских букв в слове 1<=n2<=15
-                    for (int j = 0; j < lettersInWord; j++) {
-                        word += String.valueOf(Character.forDigit(rnd.nextInt(35-10)+10,36));
+                    //Есть вероятность probability вхождения одного из слов этого массива в следующее предложение (1/probability).
+                    if (rnd.nextInt(probability) == (probability-1)) {
+                        word = additionalArray.get(rnd.nextInt(additionalArray.size()));
+                    } else {
+                        lettersInWord = rnd.nextInt(14)+1; //латинских букв в слове 1<=n2<=15
+                        for (int letter = 0; letter < lettersInWord; letter++) {
+                            word += String.valueOf(Character.forDigit(rnd.nextInt(35-10)+10,36));
+                            if ((wordCount == 0) && (letter == 0)) {
+                                word = word.toUpperCase(); // Предложение начинается с заглавной буквы
+                            }
+                        }
                     }
                     sentenceArray.add(word);
-                    sentenceArray.add(" ");
+                    if (wordCount < (wordsInSentence-1)) {
+                        switch (rnd.nextInt(5)) {
+                            case 0: //В предложении после произвольных слов могут находиться запятые
+                                sentenceArray.add(", "); 
+                                break;
+                            default:
+                                sentenceArray.add(" ");
+                        }
+                    }
                 }
-                sentenceArray[0] = sentenceArray[0].toUpperCase; // Предложение начинается с заглавной буквы
-                sentenceArray.remove(sentenceArray.size()); //удаляем последний пробел
-                switch (rnd.nextInt(4)) { // Предложение заканчивается (.|!|?)+" "
+
+                switch (rnd.nextInt(5)) { // Предложение заканчивается (.|!|?)+" "
                     case 0:
-                        sentenceArray.add("! ");
+                        sentenceArray.add("!");
                         break;
                     case 1:
-                        sentenceArray.add("? ");
+                        sentenceArray.add("?");
                         break;
                     default:
-                        sentenceArray.add(". ");
+                        sentenceArray.add(".");
+                }
+                if (sent < sentenceInParagrarh-1) { //добавил, чтобы абзац не заканчивался на пробел
+                    sentenceArray.add(" ");
                 }
             }
-            sentenceArray.remove(sentenceArray.size()); //удаляем последний пробел
             sentenceArray.add("\n\r"); // В конце абзаца стоит разрыв строки и перенос каретки.
         }
         // создание параграфов
+        for (String el : sentenceArray) {
+            System.out.print(el);
+        }
     }
         
 }

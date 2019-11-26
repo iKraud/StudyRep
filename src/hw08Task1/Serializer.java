3c029b7a3cdde4d7954dea5485e64669a96bf640
iKraud <57291604+iKraud@users.noreply.github.com>
2019-11-26 17:26:10 +0300

    Update Serializer.java

--- src/hw08Task1/Serializer.java
+++ src/hw08Task1/Serializer.java
@@ -2,71 +2,108 @@ package hw08Task1;
 
 import java.io.*;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Serializer {
-    public void serialize (Object object, String file) throws NoSuchFieldException {
+    public void serialize(Object object, String file) throws NoSuchFieldException, IllegalAccessException {
+        String info = "";
+        Class className = object.getClass();
+        info += "class:" + className  + ";";
 
-//        object.getClass().getDeclaredFields();
-//        object.getClass().getDeclaredMethods();
-//        object.getClass().getDeclaredConstructors();
-        Field field = object.getClass().getDeclaredField("name");
+        for (Field el : object.getClass().getDeclaredFields()) {
+            Field fieldName = el;
+            el.setAccessible(true);
+            el.getType();
+            String fieldValue = el.get(object).toString();
 
-//        String text = "Hello world!"; // строка для записи
+            info += "fieldName:" + fieldName +  ";" + "fieldValue:" + fieldValue + ";";
+        }
+//        Field fieldName = object.getClass().getDeclaredField("name");
+//        fieldName.setAccessible(true);
+//        String fieldValue = (String) fieldName.get(object);
+//
+//        info += ";fieldName:" + fieldName + ";fieldValue:" + fieldValue + ";";
         try (FileOutputStream fos = new FileOutputStream(file);
-             BufferedOutputStream bos = new BufferedOutputStream(fos)) {
-            byte[] buffer = field.getByte();
-            bos.write(buffer, 0, buffer.length);
+             PrintWriter pw = new PrintWriter(fos)) {
+            pw.write(info);
+            System.out.println("Объект успешно сериализован!");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-
-
-        String text = "Hello Wolrd!";
-        byte[] buffer = text.getBytes();
-        try{
-            fos.write(buffer);
+    public Object deSerialize(String file) throws IOException, IllegalAccessException {
+        Worker worker = new Worker();
+        BufferedReader fis = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF8"));
+        String s = "";
+        while (fis.ready()) {
+            s += fis.readLine();
         }
-        catch(Exception ex){
-
-            System.out.println(ex.getMessage());
+        fis.close();
+        int searchBigin;
+        int searchEnd;
+        String searchResult;
+        Field fieldToFill = null;
+        List<String> parts = new ArrayList<>() {
+            {
+                add("class:");
+                add("fieldName:");
+                add("fieldValue:");
+            }
+        };
+        List<Integer> whereIsClass = new ArrayList<>();
+        int numberOfClass = 0;
+        for (int i = 0; i < (s.length() - "class:".length()); i++) {
+            if (s.substring(i, i + "class:".length()).equals("class:")) {
+                i += "class:".length();
+                whereIsClass.set(numberOfClass, i);
+                numberOfClass ++;
+            }
         }
 
-            System.out.println("Объект успешно сериализован!");
-
-
-// с Serializable
-//        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
-//            oos.writeObject(object);
-//            System.out.println("Объект успешно сериализован!");
+        for (int i = 0; i < parts.size(); i++) {
+            searchBigin = s.indexOf(parts.get(i)) + parts.get(i).length();
+            searchEnd = s.indexOf(";", searchBigin);
+            searchResult = s.substring(searchBigin, searchEnd);
+            switch (i) {
+                case 0: // class
+                   if (worker.getClass().toString().equals(searchResult)) {
+                       System.out.println("класс совпадает");
+                   } else {
+                       System.out.println("Ошибочный класс. Десериализация отменена!");
+                       return null;
+                   }
+                    break;
+                case 1: //fieldName
+                    for (Field el : worker.getClass().getDeclaredFields()) {
+                        if (el.toString().equals(searchResult)) {
+                            fieldToFill = el;
+                            System.out.println("поле найдено");
+                        }
+                    }
+                    break;
+                case 2: //fieldValue
+                    fieldToFill.setAccessible(true);
+                    fieldToFill.set(worker, searchResult);
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + i);
+            }
+        }
+//        try (FileInputStream fis = new FileInputStream(file);
+//             BufferedInputStream bis = new BufferedInputStream(fis)) {
+//            byte[] info = bis.readAllBytes();
+//            int ch;
+//            while((bis.read())!=-1) {
+//
+//            }
+//
+//            System.out.println("Объект успешно ДЕсериализован!");
+//
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
+        return worker;
     }
-
-    public Object deSerialize (String file) {
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))){
-            Object object = ois.readObject();
-            System.out.println("Объект успешно ДЕсериализован!");
-            return object;
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    //        MyObject myObject = new MyObject(1, 'c', "args");
-//        Field[] fields = myObject.getClass().getDeclaredFields();
-//        for (Field el: fields) {
-//            System.out.println(el.getName() + " " + el.getType() + " " + el.getModifiers());
-//        }
-//        Method[] methods = myObject.getClass().getDeclaredMethods();
-//        for (Method el: methods) {
-//            System.out.println(el.getExceptionTypes());
-//        }
-//        Class c = Class.forName("MyObject");
-//        Object object = c.newInstance();
-//        MyObject myObject1 = (MyObject) object;
 }

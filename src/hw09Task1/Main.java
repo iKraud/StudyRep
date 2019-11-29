e89b85541a1e410a1f011d802ebb09059822ff8b
iKraud <57291604+iKraud@users.noreply.github.com>
2019-11-29 17:23:42 +0300

    Update Main.java

--- src/hw09Task1/Main.java
+++ src/hw09Task1/Main.java
@@ -1,5 +1,10 @@
 package hw09Task1;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Дан интерфейс
  *
@@ -16,7 +21,26 @@ package hw09Task1;
  * Метод, введенный с консоли, исполняется в рантайме (вызывается у экземпляра объекта подгруженного класса)
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String s = "";
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in)); // для чтения консоли
+        String currentString = "";
+        while (!s.endsWith("\n\n")) {
+            currentString = in.readLine();
+            s += currentString + "\n";
+        }
+        s = s.substring(0, s.length()-("\n\n").length());
 
+//        FileOutputStream fos = new FileOutputStream("C:\\Java\\IdeaProjects\\untitled\\src\\hw09Task1\SomeClass.java");
+        FileOutputStream fos = new FileOutputStream("R:\\ССиОР\\СРТиС\\03 - Разработка\\Тимохин И.В\\055_IdeaProjects\\src\\hw09Task1\\SomeClass.java");
+        fos.write(s.getBytes());
+        fos.close();
+        System.out.println("Файл класса успешно создан!");
     }
+}
+
+package hw09Task1;
+
+public interface Worker {
+    void doWork();
 }

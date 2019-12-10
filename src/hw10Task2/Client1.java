a8df12a5719b5dc6016c1512bba890bcf243c0b3
iKraud <57291604+iKraud@users.noreply.github.com>
2019-12-10 13:54:51 +0300

    Update Client1.java

--- src/hw10Task2/Client1.java
+++ src/hw10Task2/Client1.java
@@ -1,4 +1,4 @@
-package hw10Task2;
+package hw10Task1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,52 +8,54 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class Client1 {
-    public static String clientAddress = "127.0.0.1";  //"230.0.0.0" //"127.0.0.1"
     public static void main (String[] args) throws IOException {
-        MulticastSocket socket;
-        String incomingMessage;
-        String outcomingMessage;
-        String myName;
+        String clientAddress = "192.168.0.11";
+        String inMessage;
+        String outMessage;
+        String recipientName;
         InetAddress group = InetAddress.getByName("230.0.0.0"); //255.255.255.255
 
         BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
 
+        //[senderAddress@recipientName]message - образец послания
         System.out.print("Для подключения укажите ваше имя: ");
-        myName = "[" + br.readLine() + "]";
-        byte[] b = myName.getBytes();
+        outMessage = "[" + clientAddress + "@Server" + "]" + br.readLine();
 
-        socket = new MulticastSocket(Server.PORT);
+        MulticastSocket socket = new MulticastSocket(Server.MULTI_PORT);
         socket.joinGroup(group);
 
-        DatagramPacket dp = new DatagramPacket(b, b.length, InetAddress.getByName(clientAddress), Server.PORT);
+        DatagramPacket dp = new DatagramPacket(outMessage.getBytes(), outMessage.getBytes().length, InetAddress.getByName("localhost"), Server.MULTI_PORT);
         socket.send(dp);
 
         byte[] buffer = new byte[65536];
         DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
         socket.receive(reply);
         byte[] data = reply.getData();
-        incomingMessage = new String(data, 0, reply.getLength());
-        System.out.println(incomingMessage);
+        inMessage = new String(data, 0, reply.getLength());
+        System.out.println(inMessage);
 
+        MultiIncomeThread inThread = new MultiIncomeThread();
+        inThread.start();
+        UniIncomeThread uniIncomeThread = new UniIncomeThread();
+        uniIncomeThread.start();
         try {
             while (true) {
-                outcomingMessage = br.readLine();
-                b = outcomingMessage.getBytes();
-
-                dp = new DatagramPacket(b, b.length, InetAddress.getByName(clientAddress), Server.PORT);
+                outMessage = br.readLine();
+                if (outMessage.startsWith("@")) {
+                    recipientName = outMessage.substring(1, outMessage.indexOf(" "));
+                    outMessage = outMessage.substring(outMessage.indexOf(" ") + 1);
+                } else {
+                    recipientName = "Server";
+                }
+                outMessage = "[" + clientAddress + "@" + recipientName + "]" + outMessage;
+                dp = new DatagramPacket(outMessage.getBytes(), outMessage.getBytes().length, InetAddress.getByName("localhost"), Server.MULTI_PORT);
                 socket.send(dp);
 
-                buffer = new byte[65536];
-                reply = new DatagramPacket(buffer, buffer.length);
-
-                socket.receive(reply);
-                data = reply.getData();
-                incomingMessage = new String(data, 0, reply.getLength());
-                System.out.println(incomingMessage);
-
-                if (outcomingMessage.equals("quit")) {
-                    socket.close();
+                if (outMessage.substring(outMessage.indexOf("]") + 1).equals("quit")) {
+                    inThread.interrupt();
+                    uniIncomeThread.interrupt();
                     socket.leaveGroup(group);
+                    socket.close();
                     break;
                 }
             }

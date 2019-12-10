9749323c6bf80014efe5a936b6c3beac1d2e279f
iKraud <57291604+iKraud@users.noreply.github.com>
2019-12-10 14:04:33 +0300

    Update UniIncomeThread.java

--- src/hw10Task2/UniIncomeThread.java
+++ src/hw10Task2/UniIncomeThread.java
@@ -1,27 +1,27 @@
-package hw10Task1;
-
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-
-class UniIncomeThread extends Thread {
-    private byte[] buffer = new byte[65536];
-
-    public void run() {
-        DatagramSocket socket = null;
-        while (!UniIncomeThread.interrupted()) {
-            try {
-                socket = new DatagramSocket(Server.UNI_PORT);
-                DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-
-                socket.receive(reply);
-                byte[] data = reply.getData();
-                String incomingMessage = new String(data, 0, reply.getLength());
-                System.out.println(incomingMessage);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        socket.close();
-    }
-}
+//package hw10Task1;
+//
+//import java.io.IOException;
+//import java.net.DatagramPacket;
+//import java.net.DatagramSocket;
+//
+//class UniIncomeThread extends Thread {
+//    private byte[] buffer = new byte[65536];
+//
+//    public void run() {
+//        DatagramSocket socket = null;
+//        while (!UniIncomeThread.interrupted()) {
+//            try {
+//                socket = new DatagramSocket();
+//                DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+//
+//                socket.receive(reply);
+//                byte[] data = reply.getData();
+//                String incomingMessage = new String(data, 0, reply.getLength());
+//                System.out.println(incomingMessage);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        socket.close();
+//    }
+//}

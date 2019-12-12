573286f8b8c1ddd69de87f17d3223c0c8f88dfd6
iKraud <57291604+iKraud@users.noreply.github.com>
2019-12-12 11:34:56 +0300

    Update Main.java

--- src/hw12Task1/Main.java
+++ src/hw12Task1/Main.java
@@ -1,7 +1,5 @@
 package hw12Task1;
 
-import hw09Task1.MyClassLoader;
-
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import java.io.File;
@@ -17,45 +15,32 @@ import java.util.Random;
  *
  * Задание 2. Доработать программу так, чтобы ошибка OutOfMemoryError возникала в Metaspace /Permanent Generation
  */
-//-XX:MetaspaceSize=1m -XX:MaxMetaspaceSize=10m -XX:MinMetaspaceFreeRatio=50 -XX:MaxMetaspaceFreeRatio=80
+//-XX:MetaspaceSize=1m -XX:MaxMetaspaceSize=10m
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        List<MyClass> arr = new ArrayList<>();
-//        List<String> arr = new ArrayList<>();
-//        String string = "";
-        int total = 10;
-        Random rnd = new Random();
-
-//        File sourceFile = new File("C:\\Java\\IdeaProjects\\untitled\\src\\hw12Task1\\MyClass.java"); //компиляция на ходу
-//        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler(); //компиляция на ходу - можно было использовать javac
-//        compiler.run(null, null, null, sourceFile.getPath()); //компиляция на ходу
-
-        for (int i = 0; i < 10; i++) {
-            MyThread myThread= new MyThread();
-            myThread.start();
+    public static void main(String[] args) {
+        option(2);
+    }
+    static void option(int x) {
+        switch (x) {
+            case 1:
+                List<String> arr = new ArrayList<>();
+                String string = "";
+                int total = 10;
+                Random rnd = new Random();
+                for (int i = 0; i < total; i++) {
+                    total++;
+                    arr.add(string + i);
+                    if ((arr.size() % (rnd.nextInt(total) + 1)) == 0) {
+                        arr.remove(i);
+                        i--;
+                    }
+                }
+                break;
+            default:
+//                File sourceFile = new File("C:\\Java\\IdeaProjects\\untitled\\src\\hw12Task1\\MyClass.java"); //компиляция на ходу
+                File sourceFile = new File("R:\\ССиОР\\СРТиС\\03 - Разработка\\Тимохин И.В\\055_IdeaProjects\\src\\hw12Task1\\MyClass.java"); //компиляция на ходу
+                JavaCompiler compiler = ToolProvider.getSystemJavaCompiler(); //компиляция на ходу - можно было использовать javac
+                compiler.run(null, null, null, sourceFile.getPath()); //компиляция на ходу
         }
-
-//        for (int i = 0; i < total; i++) {
-//            total++;
-////---------------------------------------
-//            //#0
-////            MyClass cls = new MyClass();
-//            //#1
-//            MyClass cls = (MyClass) ClassLoader.getSystemClassLoader().loadClass("hw12Task1.MyClass").newInstance();
-//            //#2
-////            MyClass cls = (MyClass) Class.forName("hw12Task1.MyClass").newInstance();
-//            //#3
-////            ClassLoader classLoader = new MyClassLoader();
-////            Class<?> cls = Class.forName("hw12Task1.MyClass", true, classLoader);
-////---------------------------------------
-////            arr.add(string + i);
-//            arr.add(cls);
-//
-//            if ((arr.size() % (rnd.nextInt(total) + 1)) == 0) {
-//                arr.remove(i);
-//                i--;
-//            }
-//
-//        }
     }
 }

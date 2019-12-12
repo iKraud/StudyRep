47bcf00af6b3192ddcc7feaba012a5533a9d247e
iKraud <57291604+iKraud@users.noreply.github.com>
2019-12-12 11:35:15 +0300

    Update MyClass.java

--- src/hw12Task1/MyClass.java
+++ src/hw12Task1/MyClass.java
@@ -2,20 +2,4 @@ package hw12Task1;
 
 public class MyClass {
     MyClass(){}
-    String string1 = new String("1");
-    String string2 = new String("2");
-    String string3 = new String("3");
-    String string4 = new String("4");
-    String string5 = new String("5");
-    String string6 = new String("6");
-    String string7 = new String("7");
-    String string8 = new String("8");
-    String string9 = new String("9");
-    String string10 = new String("10");
-    String string11 = new String("11");
-    String string12 = new String("12");
-    String string13 = new String("13");
-    String string14 = new String("14");
-    String string15 = new String("15");
-    String string16 = new String("16");
 }

1efb6525b424a5c432731423888909a3effa7742
Igor Timohin <ikraud@gmail.com>
2019-12-15 12:24:42 +0300

    Добавил ДЗ

---
+++ src/hw16Task1/Main.java
@@ -0,0 +1,14 @@
+package hw16Task1;
+
+/**
+ * @author "Timohin Igor"
+ *
+ * Взять за основу ДЗ_15,
+ * покрыть код логированием
+ * в основных блоках try покрыть уровнем INFO
+ * с исключениях catch покрыть уровнем ERROR
+ * настроить конфигурацию логера, что бы все логи записывались в БД, таблица LOGS,
+ * колонки ID, DATE, LOG_LEVEL, MESSAGE, EXCEPTION
+ */
+public class Main {
+}

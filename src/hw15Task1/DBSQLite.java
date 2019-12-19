93ab49ededee66b2604d48dee83108230ee80370
iKraud <57291604+iKraud@users.noreply.github.com>
2019-12-19 17:13:18 +0300

    Update DBSQLite.java

--- src/hw15Task1/DBSQLite.java
+++ src/hw15Task1/DBSQLite.java
@@ -1,16 +1,23 @@
 package hw15Task1;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DBSQLite {
+    private static Logger logger = Logger.getLogger(DBSQLite.class.getName());
     public Connection connect() {
         Connection connection = null;
         try {
             Class.forName("org.sqlite.JDBC");
             String url = "jdbc:sqlite:Inno.db";
             connection = DriverManager.getConnection(url);
+            logger.info("Соединение создано...");
         } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+            logger.logp(Level.WARNING,
+                    e.getStackTrace()[1].getClassName(),
+                    e.getStackTrace()[1].getMethodName(),
+                    "Ошибка при создании соединения!");
         }
         return connection;
     }
@@ -40,8 +47,12 @@ public class DBSQLite {
                     "id integer primary key autoincrement, \n" +
                     "user_id integer references USER(id) not null, \n" +
                     "role_id integer references ROLE(id) not null);");
+            logger.info("Исходные таблицы обновлены...");
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.logp(Level.WARNING,
+                    e.getStackTrace()[1].getClassName(),
+                    e.getStackTrace()[1].getMethodName(),
+                    "Ошибка при обновлении исходных таблиц!");
         }
     }
 
@@ -56,15 +67,20 @@ public class DBSQLite {
             preparedStatement.setString(6, email);
             preparedStatement.setString(7, description);
             preparedStatement.execute();
+            logger.info("Использован Prepared Statement...");
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.logp(Level.WARNING,
+                    e.getStackTrace()[1].getClassName(),
+                    e.getStackTrace()[1].getMethodName(),
+                    "Ошибка при использовании Prepared Statement!");
         }
     }
 
     public void insertBatchToUserRole (Connection cn, int userID, int roleSum) {
         ResultSet resultSet;
         try (Statement statement = cn.createStatement();
-             PreparedStatement preparedStatement = cn.prepareStatement("insert into USER_ROLE (user_id, role_id) values (" + userID + ", ?);" )) {
+             PreparedStatement preparedStatement = cn.prepareStatement(
+                     "insert into USER_ROLE (user_id, role_id) values (" + userID + ", ?);" )) {
             int colCount = statement.executeQuery("select count(*) from ROLE").getInt(1);
             for (int i = 1; i <= colCount; i++) {
                 resultSet = statement.executeQuery("select description from ROLE where id =" + i);
@@ -76,23 +92,31 @@ public class DBSQLite {
                 }
             }
             preparedStatement.executeBatch();
+            logger.info("Использован Batch...");
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.logp(Level.WARNING,
+                    e.getStackTrace()[1].getClassName(),
+                    e.getStackTrace()[1].getMethodName(),
+                    "Ошибка при использовании Batch!");
         }
     }
 
-    public void selectFromUser (Connection cn, int loginID, String name) {
-        try (PreparedStatement preparedStatement = cn.prepareStatement("select * from USER where login_ID=? and name=?")) {
-            preparedStatement.setInt(1, loginID);
-            preparedStatement.setString(2, name);
+    public void selectFromUser (Connection cn, String name, int loginID) {
+        try (PreparedStatement preparedStatement = cn.prepareStatement("select * from USER where name=? and login_ID=?")) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, loginID);
             ResultSet resultSet = preparedStatement.executeQuery();
             resultSet.next();
             int columnCount = resultSet.getMetaData().getColumnCount();
             for (int i = 1; i <= columnCount; i++) {
-                System.out.println("Столбец: " + resultSet.getMetaData().getColumnName(i) + " = " + resultSet.getString(i));
+                System.out.println("Столбец " + resultSet.getMetaData().getColumnName(i) + " = " + resultSet.getString(i));
             }
+            logger.info("Выполнен поиск пользователя по имени и loginID");
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.logp(Level.WARNING,
+                    e.getStackTrace()[1].getClassName(),
+                    e.getStackTrace()[1].getMethodName(),
+                    "Ошибка при поиске пользователя по имени и loginID!");
         }
     }
 }
